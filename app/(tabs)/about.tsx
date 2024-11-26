8ee13fc9f16d3e24a0f631cf794d0ab12f545449
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2024-11-27 00:24:29 +0400

    feat: implemented tab based routing

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,10 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -13,15 +12,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
   text: {
-    color: "#fff",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
     color: "#fff",
   },
 });
